15790b202c6efd503bd5a1094e620dc354f3ecfd
CHAOS2614 <37375653+CHAOS2614@users.noreply.github.com>
2021-12-13 22:18:30 +0800

    init

---
+++ src/test/java/cn/edu/bjfu/redislearn/RedisLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.bjfu.redislearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
